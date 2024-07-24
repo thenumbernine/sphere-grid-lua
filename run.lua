c5a0ade741c221bd05087e05cd24567f1aebba0f
thenumbernine <christopher.e.moore@gmail.com>
2024-07-24 16:58:37 +0600

    made gles friendly

--- run.lua
+++ run.lua
@@ -6,11 +6,31 @@ local gl = require 'gl'
 local vec3d = require 'vec-ffi.vec3d'
 
 local App = require 'imguiapp.withorbit'()
-
+App.viewUseBuiltinMatrixMath = true
 App.title = 'sphere grids'
 
 function App:initGL()
 	App.super.initGL(self)
+
+	self.shader = require 'gl.program'{
+		version = 'latest',
+		precision = 'best',
+		vertexCode = [[
+in vec3 vertex;
+uniform mat4 mvProjMat;
+void main() {
+	gl_Position = mvProjMat * vec4(vertex, 1.);
+}
+]],
+		fragmentCode = [[
+out vec4 fragColor;
+void main() {
+	fragColor = vec4(1., 1., 1., 1.);
+}
+]],
+	}:useNone()
+
+	self:updateGrid()
 end
 
 local function basisFor(v)
@@ -36,8 +56,8 @@ local function basisFor(v)
 end
 
 numCircleDivs = 100	-- num circle divs
-angleMax = 120
-angleDivs = 12
+gridAngleMax = 120
+numGridLines = 12
 gridBasisType = 1
 
 local gridBasisTypes = table{
@@ -68,38 +88,55 @@ local basisForGrid = {
 	},
 }
 
-function App:update()
-	gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT))
-
-	local basis = basisForGrid[gridBasisTypes[gridBasisType]]
-
-	for _,a1 in ipairs(basis) do
+function App:updateGrid()
+	self.sceneobjs = table()
+	for _,a1 in ipairs(basisForGrid[gridBasisTypes[gridBasisType]]) do
 		local a2 = basisFor(a1):normalize()
 		local a3 = a1:cross(a2)
 		-- re-ortho-normalize a3
-		for j=1,angleDivs do
-			local phi = j/angleDivs*angleMax
+		for j=1,numGridLines do
+			local phi = j/numGridLines*gridAngleMax
 			local z = math.cos(math.rad(phi))
 			local r = math.sin(math.rad(phi))
-			
-			gl.glBegin(gl.GL_LINE_LOOP)
+
+			local vertexes = table()
 			for i=1,numCircleDivs do
 				local th = (i-.5)/numCircleDivs*2*math.pi
 				local v = a1 * z + a2 * r*math.cos(th) + a3 * r*math.sin(th)
-				gl.glVertex3dv(v.s)
+				vertexes:append{v:unpack()}
 			end
-			gl.glEnd()
+
+			self.sceneobjs:insert(require 'gl.sceneobject'{
+				program = self.shader,
+				vertexes = {
+					data = vertexes,
+					dim = 3,
+				},
+				geometry = {
+					mode = gl.GL_LINE_LOOP,
+				},
+			})
 		end
 	end
+end
 
+function App:update()
+	gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT))
+	for _,sceneobj in ipairs(self.sceneobjs) do
+		sceneobj.uniforms.mvProjMat = self.view.mvProjMat.ptr
+		sceneobj:draw()
+	end
 	App.super.update(self)
 end
 
 function App:updateGUI()
-	ig.luatableInputInt('num divs', _G, 'numCircleDivs')
-	ig.luatableInputFloat('angle max', _G, 'angleMax')
-	ig.luatableInputFloat('angle divs', _G, 'angleDivs')
-	ig.luatableCombo('grid basis', _G, 'gridBasisType', gridBasisTypes)
+	if ig.luatableInputInt('circle resolution', _G, 'numCircleDivs')
+	or ig.luatableInputFloat('grid angle max', _G, 'gridAngleMax')
+	or ig.luatableInputInt('num grid lines', _G, 'numGridLines')
+	or ig.luatableCombo('grid basis', _G, 'gridBasisType', gridBasisTypes)
+	then
+		self:updateGrid()
+	end
 end
 
 return App():run()
